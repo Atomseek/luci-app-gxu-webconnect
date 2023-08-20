5db7030e32904eb164d9e4aa368fe9bd5ae764e3
zizzzzzzzzz <67729048+Atomseek@users.noreply.github.com>
2023-08-20 23:35:11 +0800

    修改进程查询命令

--- luasrc/controller/gxu-webconnect.lua
+++ luasrc/controller/gxu-webconnect.lua
@@ -10,7 +10,7 @@ end
 
 function act_status()
 	local e = {}
-	e.running = luci.sys.call("ps -ef | grep gxuwc | grep -v grep >/dev/null") == 0
+	e.running = luci.sys.call("ps | grep gxuwc | grep -v grep >/dev/null") == 0
 	luci.http.prepare_content("application/json")
 	luci.http.write_json(e)
 end
